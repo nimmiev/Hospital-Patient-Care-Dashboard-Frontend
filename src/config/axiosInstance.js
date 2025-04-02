541ca3ce8b31c83211481c3753536c0a416d481c
nimmiev <nimmiev222@gmail.com>
2025-04-02 11:45:04 +0530

    update url

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 const API_URL = import.meta.env.VITE_API_URL
 
-// console.log(API_URL)
+console.log(API_URL)
 
 export const axiosInstance = axios.create({baseURL: API_URL, withCredentials: true})
